fe3721bd5799c84d50e5b0384c00df0c07b0e8ca
the-one-monster <god.s.m.o.97@gmail.com>
2025-11-16 06:56:29 +0530

    backend

--- backend/.venv/bin/activate.fish
+++ backend/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shaikmohammedomar/XCodenovate/asdasdasdasdasdasdasds/backend/.venv"
+set -gx VIRTUAL_ENV "/Users/shaikmohammedomar/XCodenovate/Habituate/backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
